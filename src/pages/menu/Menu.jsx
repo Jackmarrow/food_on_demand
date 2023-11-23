1c598f48728d220f7325869236496df5e78a0025
saidlionsgeek <saidlionesgeek.1997@gmail.com>
2023-11-23 16:22:12 +0100

    packages

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import pizza from '../../pizza1.png'
+import FlyingButton from 'react-flying-item'
+import toast, { Toaster } from 'react-hot-toast'
+const notify = () => toast.success('Here is your toast.');
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <>
+    <div>
+      <img src={pizza} alt='' />
+      <FlyingButton targetLeft={'90%'} src={[pizza]}>fly</FlyingButton>
+
+      <div>
+      <button onClick={notify}>Make me a toast</button>
+      <Toaster />
+    </div>
+    </div>
+    </>
   )
 }
 
---
+++ text.md
@@ -0,0 +1,3 @@
+plaette de couleur 
+couleur principale : #d6eae1
+coleur secondaire (pour les button ect): 
